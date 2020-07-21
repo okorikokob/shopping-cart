864a362bf4606a88da19ee4d46356afcc3c68255
Okorikoko Blessing <okorikokob@gmail.com>
2020-07-21 20:18:10 +0100

    firstcomit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Shopping Cart
+
+# create the app
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
